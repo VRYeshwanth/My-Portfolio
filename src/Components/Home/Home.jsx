239f252b5c52b21ae2174cefae92e736fc7631f0
Yeshwanth <vryeshwanth06@gmail.com>
2025-09-06 15:08:34 +0530

    Home Section + Styling

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import LeftSection from "./LeftSection"
+import RightSection from "./RightSection"
+
+export default function Home() {
+    return (
+        <section className="home" id="home">
+            <LeftSection />
+        </section>
+    )
+}
---
+++ src/Components/Home/LeftSection.jsx
@@ -0,0 +1,9 @@
+export default function LeftSection() {
+    return (
+        <div className="left-section">
+            <h1>Hi, I'm VR Yeshwanth</h1>
+            <h2>Aspiring Web Developer learning React</h2>
+            <button className="cta">View Projects</button>
+        </div>
+    )
+}
