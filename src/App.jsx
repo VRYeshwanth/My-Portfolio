e3ed4c138f1736fd3c1ef990482041c027c74e7e
Yeshwanth <vryeshwanth06@gmail.com>
2025-09-06 19:49:02 +0530

    Basic Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 export default function App() {
     return (
         <div className="container">
             <Navbar />
             <Home />
+            <Footer />
         </div>
     )
 }
