49163cab34b29dd336a7d6dce5898d1d46431d6d
Yeshwanth <vryeshwanth06@gmail.com>
2025-11-28 11:38:49 +0530

    Code Refactoring

--- src/Components/About/Content.jsx
+++ src/Components/About/Content.jsx
@@ -2,11 +2,19 @@ export default function Content() {
     return (
         <div className="about-content">
             <div className="left">
-                <img src="/images/user-image.png" alt="User Image" />
+                <img
+                    src="/My-Portfolio/images/user-image.png"
+                    alt="User Image"
+                />
             </div>
             <div className="right">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium saepe earum excepturi corrupti provident, dolor numquam at dolorum. Inventore perferendis, repudiandae ut fugit laudantium ab impedit id dignissimos vero ipsa?</p>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Laudantium saepe earum excepturi corrupti provident, dolor
+                    numquam at dolorum. Inventore perferendis, repudiandae ut
+                    fugit laudantium ab impedit id dignissimos vero ipsa?
+                </p>
             </div>
         </div>
-    )
+    );
 }
--- src/Components/About/SkillIcon.jsx
+++ src/Components/About/SkillIcon.jsx
@@ -1,11 +1,15 @@
-import React, {useState} from "react"
+import { useState } from "react";
 
 export default function SkillIcon(props) {
     const [hovered, setHovered] = useState(false);
     return (
-        <div className={`skill-icon ${hovered ? "hovered" : ""}`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+        <div
+            className={`skill-icon ${hovered ? "hovered" : ""}`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
             <i className={props.class}></i>
             {hovered && <p className="icon-name">{props.name}</p>}
         </div>
-    )
+    );
 }
